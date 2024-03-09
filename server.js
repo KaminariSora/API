c2eef3e9a73dc3687c50d5f103aefaef8b7f3267
Your NameKaminariSora <nonthacha.h@gmail.com>
2024-03-09 13:50:46 +0700

    update

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 var express = require('express')
 var cors = require('cors')
 var app = express()
- 
+
+// get the client
+const mysql = require('mysql2');
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root ',
+  port : '4306',
+  database: 'travel',
+});
+
 app.use(cors())
- 
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
+
+app.get('/api/attractions', function (req, res, next) {
+    // simple query
+    connection.query(
+        'SELECT * FROM `attractions` LIMIT 0, 10',
+        function (err, results, fields) {
+            console.log(results); // results contains rows returned by server
+            res.json({result : results})
+        }
+    );
 })
- 
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
+
+app.listen(5000, function () {
+    console.log('CORS-enabled web server listening on port 5000')
 })
